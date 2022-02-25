7d3adac796380b7f330e6f32dc963e8699761157
RUPESH RAIKWAR <96075514+RupeshRaikwar@users.noreply.github.com>
2022-02-26 01:24:30 +0530

    Add files via upload

---
+++ CoSchedule/CoSchedule/scripts/footer.js
@@ -0,0 +1,106 @@
+
+function footer(){
+    return `<div id="footer">
+    <div id="footer-u">
+        <div id="footer-u1">
+            <div>
+                <img id="footer-site-logo" src="https://coschedule.com/img/favicon.ico" alt="">
+                <p>Sign Up</p>
+                <p>Request A Demo</p>
+                <p>Sign In</p>
+            </div>
+            <div>
+                <h4 id="f-tag">PRODUCTS</h4>
+                <div id="f-tag-flex">
+                    <img id="f-tag-flex-img"
+                        src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-calendar-product-icon.png&w=32&q=75"
+                        alt="">
+                    <P>Marketing Calendar</P>
+                </div>
+                <div id="f-tag-flex">
+                    <img id="f-tag-flex-img"
+                        src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-calendar-product-icon.png&w=32&q=75"
+                        alt="">
+                    <P>Marketing Suite</P>
+                </div>
+                <div id="f-tag-flex">
+                    <img id="f-tag-flex-img"
+                        src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-calendar-product-icon.png&w=32&q=75"
+                        alt="">
+                    <P>Headline Studio</P>
+                </div>
+                <div id="f-tag-flex">
+                    <img id="f-tag-flex-img"
+                        src="https://coschedule.com/_next/image?url=%2Fimg%2Fproduct%2Fmarketing-calendar-product-icon.png&w=32&q=75"
+                        alt="">
+                    <p>Actionable Marketing Institute</p>
+                </div>
+
+
+
+
+            </div>
+            <div>
+                <h4 id="f-tag">WHY COSCHEDULE</h4>
+                <P>Customer Stories</P>
+                <P>Support</P>
+                <P>Product Announcements</P>
+                <p>Pricing</p>
+            </div>
+            <div>
+                <h4 id="f-tag">COMPANY</h4>
+                <p>About</p>
+                <p>Jobs</p>
+                <p>Contact Us</p>
+                <p>Affiliate Program</p>
+                <p>Terms & Policies</p>
+            </div>
+        </div>
+        <div id="footer-u2">
+            <div></div>
+            <div>
+                <h4 id="f-tag">RESOURCES</h4>
+                <p>CoSchedule Marketing Blog</p>
+                <p>Actionable Marketing Podcast</p>
+                <p>10x Marketing Formula Book</p>
+                <p>Get Started With Agile Marketing Book</p>
+                <p>Marketing Ideas Hub</p>
+            </div>
+            <div>
+                <h4 id="f-tag">GUIDES</h4>
+                <p>CoSchedule Guide</p>
+                <p>Agile Marketing Guide</p>
+                <p>Marketing Strategy Guide</p>
+                <p>Content Marketing Guide</p>
+            </div>
+            <div>
+                <h4 id="f-tag">TOOLS</h4>
+                <p>Headline Studio</p>
+                <p>Social Message Optimizer</p>
+                <p>Email Subject Line Tester</p>
+                <p>Marketing Dictionary</p>
+            </div>
+
+        </div>
+    </div>
+    <div id="footer-l">
+        <p>© Copyright 2022, all rights reserved. See our Terms & Policies</p>
+        <div id="footer-logos">
+            <img id="footer-logo"
+                src="https://www.kindpng.com/picc/m/22-227301_facebook-logo-png-flat-clipart-image-transparent-facebook.png"
+                alt="">
+            <img id="footer-logo"
+                src="https://balancedlifeskills.com/wp-content/uploads/2017/03/twitter-logo-large-297x300.png"
+                alt="">
+            <img id="footer-logo" src="https://pngimg.com/uploads/linkedIn/linkedIn_PNG37.png" alt="">
+            <img id="footer-logo"
+                src="https://s-media-cache-ak0.pinimg.com/736x/23/ac/6f/23ac6f22d8bcff4881c8fddaaca0dc1f.jpg"
+                alt="">
+            <img id="footer-logo"
+                src="http://cdn.freelogovectors.net/wp-content/uploads/2016/12/instagram-logo-1.png" alt="">
+        </div>
+    </div>
+</div>`;
+}
+
+export default footer ;
